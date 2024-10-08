9194c4ba519a988295907b4757c942b042030031
PedroFernandesG <147552248+PedroFernandesG@users.noreply.github.com>
2024-10-07 23:28:03 -0300

    Create LinearSearch.cpp

---
+++ Generalised Algorithms/LinearSearch.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#define Size 5
+
+using namespace std;
+typedef char DataType;
+
+int linearSearch(DataType *array, int size, DataType x){
+    
+    for(int i = 0;i < size;i++){
+        if(array[i] == x)
+            return i;
+    }
+    return -1;
+}
+
+int main(void){
+
+    char x;
+    char array[Size] = {'A', 'G', 'H', 'J', 'C'};
+    
+    cout << "type in which element you want to find: " << endl;
+    cin >> x;
+    
+    return linearSearch(array,Size,x);
+}
